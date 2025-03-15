47296ae714f500eb034ad34ca6227895dd9f5894
sukhmankaur09 <sukhmankaur1202@gmail.com>
2025-03-15 09:33:32 -0400

    tax calculator endpoint

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca2fe59360ac6631b6655b19c59d763e981a27c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5dfdaa74c95153cc5176b1d1405ab476c442749")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
